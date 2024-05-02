9ea6df77ac8e05d49d23e48dea89e63d7e47d44a
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-05-02 18:01:59 +0300

    added components

--- tabletop_initiative_tracker/src/App.tsx
+++ tabletop_initiative_tracker/src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import InitiativeList from './components/InitiativeList/InitiativeList'
+import ControlBox from './components/ControlBox/ControlBox'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(): JSX.Element {
 
   return (
-    <>
-      Tabletop tracker
-    </>
+    <div className='app'>
+        <Header />
+        <div className='content'><InitiativeList /> <ControlBox /></div>
+        <Footer />
+    </div>
   )
 }
-
-export default App
